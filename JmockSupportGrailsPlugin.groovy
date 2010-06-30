585012870a7f39faa94804add34e9544a2f9d777
Marco Vermeulen <marco.vermeulen@and.co.uk>
2010-06-30 13:04:16 +0100

    Clean up plugin descriptor.

--- JmockSupportGrailsPlugin.groovy
+++ JmockSupportGrailsPlugin.groovy
@@ -2,49 +2,25 @@ class JmockSupportGrailsPlugin {
     // the plugin version
     def version = "0.1"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "1.3.2 > *"
+    def grailsVersion = "1.3.0 > *"
     // the other plugins this plugin depends on
     def dependsOn = [:]
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
-            "grails-app/views/error.gsp"
+            "grails-app/views/",
+            "web-app"
     ]
 
-    // TODO Fill in these fields
-    def author = "Your name"
-    def authorEmail = ""
-    def title = "Plugin summary/headline"
+    def author = "Marco Vermeulen"
+    def authorEmail = "marco_vermeulen@yahoo.co.uk"
+    def title = "JMock Support Plugin for Grails"
     def description = '''\\
-Brief description of the plugin.
+Adds JMock support to your Grails Application.
+Provides extensions of all the Grails Unit Test Case classes retrofitted with Mockery instances.
+Also makes the JMock checking blocks more DSL-like.
 '''
 
     // URL to the plugin's documentation
     def documentation = "http://grails.org/plugin/jmock-support"
 
-    def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional), this event occurs before 
-    }
-
-    def doWithSpring = {
-        // TODO Implement runtime spring config (optional)
-    }
-
-    def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
-    }
-
-    def doWithApplicationContext = { applicationContext ->
-        // TODO Implement post initialization spring config (optional)
-    }
-
-    def onChange = { event ->
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-    }
-
-    def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
 }
